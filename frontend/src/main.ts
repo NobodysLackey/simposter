fb2ace4fcd149eb2219f208ab5867ad2bb13788c
Michael Lackey <59581978+NobodysLackey@users.noreply.github.com>
2026-08-26 05:50:02 +0300

    Load dedicated TV editor layout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -14,6 +14,7 @@ import './services/libraryNavigationDirectUx'
 import './services/movieEditorUx'
 import './services/matteToggleUx'
 import './services/editorActionLayoutUx'
+import './services/tvEditorLayoutUx'
 import './services/viewportLayoutUx'
 import './services/synthwaveTheme'
 
